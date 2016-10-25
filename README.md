8b466ae065c32155533b246d49a5b2a896b6e377
EVE YUAN <eve.yi.yuan@gmail.com>
2016-10-25 03:30:55 -0700

    added processing code and data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # switcharoo
+
+Note, please include "google-10000-english.txt" from [here](https://github.com/first20hours/google-10000-english) in this directory
---
+++ processData.py
@@ -0,0 +1,48 @@
+import sys, getopt
+import json
+import string
+
+topWords = []
+topWordsFile = "google-10000-english.txt"
+def buildTop():
+	with open(topWordsFile, 'r') as f:
+		for line in f:
+			line = line.strip()
+			topWords.append(line)
+
+def simpleParse(source, dest):
+	fDest = open(dest, 'a')
+	with open(source, 'r') as fSource:
+		for line in fSource:
+			commentObj = json.loads(line)
+			comment = commentObj[u'body']
+			comment = str(comment)
+			comment = comment.split()
+			words = [0]*10000
+			for word in comment:
+				wordNoPunct = word.translate(string.maketrans("",""), string.punctuation)
+				if wordNoPunct.lower() in topWords:
+					words[topWords.index(wordNoPunct.lower())] += 1
+			wordsStr = ','.join(str(x) for x in words)
+			fDest.write(wordsStr)
+			fDest.write('\n')
+	fDest.close()
+
+def main(argv):
+	buildTop()
+	source = ''
+	dest = ''
+	try:
+		opts, args = getopt.getopt(argv,"i:o:")
+	except getopt.GetoptError:
+		print 'Usage: processData.py -i <input file> -o <output file>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == "-i":
+			source = arg
+		elif opt == "-o":
+			dest = arg
+	simpleParse(source, dest)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
