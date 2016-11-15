dd51fe187fe63091c79fd9af52c446972a5aafd1
Victor Han <personpersonpersonperson@gmail.com>
2016-11-14 23:33:34 -0800

    added sorting

--- pruneDataJokes.py
+++ pruneDataJokes.py
@@ -3,6 +3,9 @@ import string
 import sys, getopt
 
 def pruneData(source, dest):
+	''' Input: source - a JSON file of reddit submissions following pushshift's submission formulation
+		Output: dest - a JSON file of only reddit submissions in joke subreddits that are not 18+
+	'''
 	fDest = open(dest+".json", 'a')
 	with open(source, 'r') as f:
 		for line in f:
@@ -24,20 +27,44 @@ def pruneData(source, dest):
 
 	fDest.close()
 
+def sortByScore(source, dest):
+	''' Input: source - a JSON file of reddit submissions
+		Output: dest - Reddit submissions from source sorted by score decreasing
+	'''
+	fDest = open(dest+".json", 'a')
+	submissions = []
+	with open(source, 'r') as f:
+		for line in f:
+			commentObj = json.loads(line)
+			submissions.append(commentObj)
+
+
+	submissions = sorted(submissions, key=lambda comment: comment[u'score'], reverse=True)
+	for s in submissions:
+		fDest.write(json.dumps(s))
+		fDest.write("\n")
+	fDest.close()
+
 def main(argv):
 	source = ''
 	dest = ''
+	sort = ''
 	try:
-		opts, args = getopt.getopt(argv,"i:o:")
+		opts, args = getopt.getopt(argv,"i:o:s:")
 	except getopt.GetoptError:
-		print 'Usage: pruneDataJokes.py -i <input file> -o <output file name (no extension)>'
+		print 'Usage: pruneDataJokes.py -i <input file> -o <output file name (no extension)> -s <sort, 1 for yes, 0 for no>'
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == "-i":
 			source = arg
 		elif opt == "-o":
 			dest = arg
-	pruneData(source,dest)
+		elif opt == "-s":
+			sort = arg
+	if sort == '0':
+		pruneData(source,dest)
+	else:
+		sortByScore(source, dest)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
