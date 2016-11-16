38a1c9b37b451ba284d6b7a2280a6fb1cdd85b02
EVE YUAN <eve.yi.yuan@gmail.com>
2016-11-15 16:06:31 -0800

    Simple joke recommender using gensim's built-in wordmover distance. Needs pyemd Python library as well nltk's stopwords corpora

---
+++ wordmover.py
@@ -0,0 +1,51 @@
+# Sample Word Mover comment blaster
+
+import numpy as np
+import gensim
+import nltk
+
+class WordMover:
+	"""
+	Implements a simple joke recommendation system. Loads in a database of
+	jokes and a word2vec word embedding and uses the word mover algorithm to
+	find similar jokes.
+	"""
+
+	def __init__(self, jokes, filename = 'GoogleNews-vectors-negative300.bin', binary = True):
+		"""
+		Takes in a trained word2vec embedding from a file and a list of jokes
+		and stores them as member variables.
+		"""
+		self.model = gensim.models.Word2Vec.load_word2vec_format(filename, binary = binary)
+		self.jokes = self.tokenize(jokes)
+
+	@staticmethod
+	def tokenize(jokes):
+		stopwords = nltk.corpus.stopwords.words('english')
+		for i, joke in enumerate(jokes):
+			tokens = joke.strip().lower().split()
+			jokes[i] = [word for word in tokens if word not in stopwords]
+		return jokes
+
+	def findBest(self, comment):
+		best = None
+		closest = float('inf')
+		tokens = comment.strip().lower().split()
+		stopwords = nltk.corpus.stopwords.words('english')
+		tokens = [token for token in tokens if token not in stopwords]
+		#print self.jokes
+		for joke in self.jokes:
+			distance = self.model.wmdistance(joke, tokens)
+			if distance < closest:
+				closest = distance
+				best = joke
+		return best
+
+def test():
+	jokes = ["pizza burgers fries", "cat dog mouse"]
+	model = WordMover(jokes)
+	best = model.findBest("I like kittens")
+	print best
+
+if __name__ == '__main__':
+	test()
